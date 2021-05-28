0125ca0b06f65df42b8b58846f7b6d58342de29a
Jenova <nhatanh0603@gmail.com>
2021-05-28 20:33:23 +0700

    Init

---
+++ CRUD_MYSQL/Data/CRUD_MYSQLContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using CRUD_MYSQL.Models;
+
+namespace CRUD_MYSQL.Data
+{
+    public class CRUD_MYSQLContext : DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;port=3306;database=demo;uid=root;password=", new MySqlServerVersion(new Version(5, 7, 24)))
+                .UseLoggerFactory(LoggerFactory.Create(b => b
+                    .AddConsole()
+                    .AddFilter(level => level >= LogLevel.Information)))
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+        }
+    }
+}
---
+++ CRUD_MYSQL/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_MYSQL.Models
+{
+    public class User
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string FullName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
